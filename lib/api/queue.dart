2f370208f980a8491ac4bd22612ed425a30e203c
zhangjiayi <zhangjiayi@sinoiov.com>
2021-04-12 19:40:09 +0800

    提交

--- lib/api/queue.dart
+++ lib/api/queue.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 
 import 'package:mk_flutter_app/api/request.dart';
 
-const BASE_URL = 'http://localhost:5758';
+// const BASE_URL = 'http://localhost:5758';
+const BASE_URL = 'http://192.168.3.179:5758';
 
 final _request = HttpRequest(BASE_URL);
 
